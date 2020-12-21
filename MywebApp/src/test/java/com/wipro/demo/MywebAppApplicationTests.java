32a65392ababfc7985f813e11644c0e17622a5ee
PR20167953 <PR20167953@APL-G2QB403>
2020-12-21 16:11:13 +0530

    commit

--- MywebApp/src/test/java/com/wipro/demo/MywebAppApplicationTests.java
+++ MywebApp/src/test/java/com/wipro/demo/MywebAppApplicationTests.java
@@ -1,5 +1,7 @@
 package com.wipro.demo;
 
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class MywebAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		Assertions.assertEquals("2","2");
 	}
 
 }
