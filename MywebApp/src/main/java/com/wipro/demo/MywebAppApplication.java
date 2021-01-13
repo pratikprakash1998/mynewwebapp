45f7c582d144ae1e929bf5a4482929fb94a036eb
PR20167953 <PR20167953@APL-G2QB403>
2021-01-13 09:10:06 +0530

    first pom.xml

--- MywebApp/src/main/java/com/wipro/demo/MywebAppApplication.java
+++ MywebApp/src/main/java/com/wipro/demo/MywebAppApplication.java
@@ -2,9 +2,10 @@ package com.wipro.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MywebAppApplication {
+public class MywebAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MywebAppApplication.class, args);
